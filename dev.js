212fc0df3da68b45d50b4dca59906ba3ddeb0df8
Rendiero <workspace.rendy@gmail.com>
2025-01-08 15:12:39 +0700

    FAQ on process

---
+++ dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// document.querySelectorAll('.faq-question').forEach(button => {
+//     button.addEventListener('click', () => {
+//         const answer = button.nextElementSibling;
+//         const icon = button.querySelector('.toggle-icon');
+//         answer.classList.toggle('hidden');
+//         icon.textContent = answer.classList.contains('hidden') ? '+' : '-';
+//     });
+// });
